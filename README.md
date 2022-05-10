ecd048a30544564b3d1fb2d9fc9e2c163f4d6b0f
YatharthChauhan2362 <yc232326@gmail.com>
2022-05-10 19:27:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tic-Tac-Toe-Ultimate-App
+ 
---
+++ app/src/main/java/com/example/tictactoe_ultimate/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tictactoe_ultimate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
